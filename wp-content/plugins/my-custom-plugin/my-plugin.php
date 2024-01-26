e14f8b0de03240786b097716358050e402e1b25d
unknown <wardren@gmail.com>
2024-01-26 15:00:49 +0000

    initial commit of the plugin

---
+++ wp-content/plugins/my-custom-plugin/my-plugin.php
@@ -0,0 +1,16 @@
+<?php
+ /*
+ * Plugin Name:       My First Plugin
+ * Plugin URI:        https://github.com/InOutLuz/wordpress-theme-and-plugin-development
+ * Description:       My first plugin for the course WordPress for developers in Softuni
+ * Version:           1.0.0
+ * Requires at least: 5.0
+ * Requires PHP:      8.0
+ * Author:            Dimitar G
+ * Author URI:        https://github.com/InOutLuz/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       softuni
+ * Domain Path:       /languages
+ */
