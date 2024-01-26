f7389b45f56b66429b9649f0b025ba68e9606b6a
unknown <wardren@gmail.com>
2024-01-26 13:38:21 +0000

    Dynamised the related posts loop and category links

--- wp-content/themes/final-project-softuni/single.php
+++ wp-content/themes/final-project-softuni/single.php
@@ -5,11 +5,12 @@
     <div class="row">
       <div class="col-lg-12">
         <h3><?php the_title(); ?></h3>
-        <span class="breadcrumb"><a href="<?php echo get_home_url('/'); ?>">Home</a> > <a href="#">Blog</a> > <?php the_title(); ?></span>
+        <span class="breadcrumb"><a href="<?php echo get_home_url('/'); ?>">Home</a> > <a href="<?php echo esc_url(site_url('/blog/')); ?>">Blog</a> > <?php the_title(); ?></span>
       </div>
     </div>
   </div>
 </div>
+
 
 <?php if (have_posts()) : ?>
 
@@ -22,12 +23,12 @@
             <div class="left-image">
               <?php
               if (has_post_thumbnail()) {
-                the_post_thumbnail('medium_large'); // Medium-large resolution (default 768px x no height limit max)
+                the_post_thumbnail('full'); // Medium-large resolution (default 768px x no height limit max)
               }
               ?>
             </div>
           </div>
-          <div class="col-lg-12 align-self-center">
+          <div class="col-lg-12 align-self-center single-margin-top">
             <h4><?php the_title(); ?></h4>
             <?php the_content(); ?>
           </div>
@@ -53,8 +54,16 @@
         <div class="row">
           <div class="col-lg-6">
             <div class="section-heading">
-              <h6>Action</h6>
-              <h2>Related Games</h2>
+              <?php $categories = get_the_category();
+              $separator = ', ';
+              $output = '';
+              if (!empty($categories)) {
+                foreach ($categories as $category) {
+                  $output .= '<a class="single-post-category-link" href="' . esc_url(get_category_link($category->term_id)) . '" alt="' . esc_attr(sprintf(__('View all posts in %s', 'softuni'), $category->name)) . '">' . esc_html($category->name) . '</a>' . $separator;
+                }
+                echo trim($output, $separator);
+              } ?>
+              <h2>Related Posts</h2>
             </div>
           </div>
           <div class="col-lg-6">
@@ -62,48 +71,32 @@
               <a href="shop.html">View All</a>
             </div>
           </div>
-          <div class="col-lg col-sm-6 col-xs-12">
-            <div class="item">
-              <h4>Action</h4>
-              <div class="thumb">
-                <a href="product-details.html"><img src="assets/images/categories-01.jpg" alt=""></a>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg col-sm-6 col-xs-12">
-            <div class="item">
-              <h4>Action</h4>
-              <div class="thumb">
-                <a href="product-details.html"><img src="assets/images/categories-05.jpg" alt=""></a>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg col-sm-6 col-xs-12">
-            <div class="item">
-              <h4>Action</h4>
-              <div class="thumb">
-                <a href="product-details.html"><img src="assets/images/categories-03.jpg" alt=""></a>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg col-sm-6 col-xs-12">
-            <div class="item">
-              <h4>Action</h4>
-              <div class="thumb">
-                <a href="product-details.html"><img src="assets/images/categories-04.jpg" alt=""></a>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg col-sm-6 col-xs-12">
-            <div class="item">
-              <h4>Action</h4>
-              <div class="thumb">
-                <a href="product-details.html"><img src="assets/images/categories-05.jpg" alt=""></a>
-              </div>
-            </div>
-          </div>
+          <?php
+          $related_categories = wp_get_post_categories(get_the_ID()); // Get categories of the current post
+
+          $args = array(
+            'post_type'      => 'post',
+            'posts_per_page' => 4,
+            'category__in' => $related_categories, // Only include posts from related categories
+            'post__not_in' => array(get_the_ID()), // Exclude the current post from the related posts
+          );
+
+          $query = new WP_Query($args);
+
+          if ($query->have_posts()) :
+
+
+            while ($query->have_posts()) : $query->the_post();
+              get_template_part('template-parts/post-content', 'post-content');
+            endwhile;
+            wp_reset_postdata(); // Reset the post data
+          else :
+            echo '<h1>Sorry, there aren\'t any related posts yet.</h1>';
+          endif;
+          ?>
         </div>
       </div>
+    </div>
     </div>
 
     <?php get_footer(); ?>
