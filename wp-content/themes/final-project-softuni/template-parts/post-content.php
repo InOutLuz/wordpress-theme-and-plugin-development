193ea853750070002d1bcbf607806902d579c5d9
unknown <wardren@gmail.com>
2024-01-30 15:00:19 +0000

    added default thumbnail photo for the post cards if there isn't one so it does not break the structure of the card

--- wp-content/themes/final-project-softuni/template-parts/post-content.php
+++ wp-content/themes/final-project-softuni/template-parts/post-content.php
@@ -2,9 +2,12 @@
     <div class="item">
         <div class="thumb">
             <?php
-            if (has_post_thumbnail()) {
+            //display the post thumbnail if there isn't one display the awaiting image photo
+             if ( has_post_thumbnail() ) {
                 the_post_thumbnail();
-            }
+                } else { ?>
+                <img src="<?php echo get_home_url('/'); ?>/wp-content/themes/final-project-softuni/assets/images/awaiting-image.jpg" alt="<?php the_title(); ?>" />
+                <?php } 
             ?>
         </div>
         <div class="down-content">
@@ -17,7 +20,8 @@
                         $output .= '<a href="' . esc_url(get_category_link($category->term_id)) . '" alt="' . esc_attr(sprintf(__('View all posts in %s', 'softuni'), $category->name)) . '">' . esc_html($category->name) . '</a>' . $separator;
                     }
                     echo trim($output, $separator);
-                } ?></span>
+                } ?>
+                </span>
             <a href="<?php the_permalink(); ?>">
                 <h4><?php the_title(); ?></h4>
             </a>
