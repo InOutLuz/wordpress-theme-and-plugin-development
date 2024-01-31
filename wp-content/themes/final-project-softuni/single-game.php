f7d423818412c37c817fadfc250d87cac079881d
unknown <wardren@gmail.com>
2024-01-31 10:58:25 +0000

    added additional information field to the single-game view using ACF

--- wp-content/themes/final-project-softuni/single-game.php
+++ wp-content/themes/final-project-softuni/single-game.php
@@ -64,31 +64,38 @@
   </div>
 </div>
 
-<div class="more-info">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="tabs-content">
-          <div class="row">
-            <div class="nav-wrapper ">
-              <ul class="nav nav-tabs" role="tablist">
-                <li class="nav-item" role="presentation">
-                  <button class="nav-link active" id="description-tab" data-bs-toggle="tab" data-bs-target="#description" type="button" role="tab" aria-controls="description" aria-selected="true">Description</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                  <button class="nav-link" id="reviews-tab" data-bs-toggle="tab" data-bs-target="#reviews" type="button" role="tab" aria-controls="reviews" aria-selected="false">Reviews (3)</button>
-                </li>
-              </ul>
-            </div>
-            <div class="tab-content" id="myTabContent">
-              <div class="tab-pane fade show active" id="description" role="tabpanel" aria-labelledby="description-tab">
-                <p>You can search for more templates on Google Search using keywords such as "templatemo digital marketing", "templatemo one-page", "templatemo gallery", etc. Please tell your friends about our website. If you need a variety of HTML templates, you may visit Tooplate and Too CSS websites.</p>
-                <br>
-                <p>Coloring book air plant shabby chic, crucifix normcore raclette cred swag artisan activated charcoal. PBR&B fanny pack pok pok gentrify truffaut kitsch helvetica jean shorts edison bulb poutine next level humblebrag la croix adaptogen. Hashtag poke literally locavore, beard marfa kogi bruh artisan succulents seitan tonx waistcoat chambray taxidermy. Same cred meggings 3 wolf moon lomo irony cray hell of bitters asymmetrical gluten-free art party raw denim chillwave tousled try-hard succulents street art.</p>
-              </div>
-              <div class="tab-pane fade" id="reviews" role="tabpanel" aria-labelledby="reviews-tab">
-                <p>Coloring book air plant shabby chic, crucifix normcore raclette cred swag artisan activated charcoal. PBR&B fanny pack pok pok gentrify truffaut kitsch helvetica jean shorts edison bulb poutine next level humblebrag la croix adaptogen. <br><br>Hashtag poke literally locavore, beard marfa kogi bruh artisan succulents seitan tonx waistcoat chambray taxidermy. Same cred meggings 3 wolf moon lomo irony cray hell of bitters asymmetrical gluten-free art party raw denim chillwave tousled try-hard succulents street art.</p>
-              </div>
+
+           
+              <?php
+                // Get the value of the text area field from ACF
+                $text_area_value = get_field('description');
+
+                // Check if the field has a value and display the additional information if it does
+                if($text_area_value) {
+                  ?>
+                  
+                  <div class="more-info">
+                   <div class="container">
+                     <div class="row">
+                       <div class="col-lg-12">
+                         <div class="tabs-content">
+                           <div class="row">
+                             <div class="nav-wrapper ">
+                               <ul class="nav nav-tabs" role="tablist">
+                                 <li class="nav-item" role="presentation">
+                                   <button class="nav-link active" id="description-tab" data-bs-toggle="tab" data-bs-target="#description" type="button" role="tab" aria-controls="description" aria-selected="true">Additional information</button>
+                                 </li>
+                               </ul>
+                             </div>
+                           <div class="tab-content" id="myTabContent">
+                           <div class="tab-pane fade show active" id="description" role="tabpanel" aria-labelledby="description-tab">
+                <?php
+               // Output the value of the text area field
+               echo '<div class="text-area-value">' . $text_area_value . '</div>';
+                }
+                ?>
+                
+                </div>
             </div>
           </div>
         </div>
