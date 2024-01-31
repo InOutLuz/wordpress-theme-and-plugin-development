bf6dc27ca997be1e654af6e48d37cc876432ab3e
unknown <wardren@gmail.com>
2024-01-31 10:04:22 +0000

    added some dynamic data to the single view for games cpt

--- wp-content/themes/final-project-softuni/single-game.php
+++ wp-content/themes/final-project-softuni/single-game.php
@@ -4,8 +4,8 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-12">
-        <h3>Modern Warfare® II</h3>
-        <span class="breadcrumb"><a href="#">Home</a> > <a href="#">Shop</a> > Assasin Creed</span>
+        <h3><?php the_title(); ?></h3>
+        <span class="breadcrumb"><a href="<?php echo get_home_url('/'); ?>">Home</a> > <a href="<?php echo esc_url(site_url('/games/')); ?>">Games</a> > <?php the_title(); ?></span>
       </div>
     </div>
   </div>
@@ -16,20 +16,44 @@
     <div class="row">
       <div class="col-lg-6">
         <div class="left-image">
-          <img src="assets/images/single-game.jpg" alt="">
+        <?php
+            //display the post thumbnail if there isn't one display the awaiting image photo
+             if ( has_post_thumbnail() ) {
+                the_post_thumbnail();
+                } else { ?>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/awaiting-image.jpg" alt="<?php the_title(); ?>" />
+                <?php } 
+            ?>
         </div>
       </div>
       <div class="col-lg-6 align-self-center">
-        <h4>Call of Duty®: Modern Warfare® II</h4>
+        <h4><?php the_title(); ?></h4>
         <span class="price"><em>$28</em> $22</span>
-        <p>LUGX Gaming Template is based on the latest Bootstrap 5 CSS framework. This template is provided by TemplateMo and it is suitable for your gaming shop ecommerce websites. Feel free to use this for any purpose. Thank you.</p>
+        <p><?php the_content(); ?></p>
         <form id="qty" action="#">
           <input type="qty" class="form-control" id="1" aria-describedby="quantity" placeholder="1">
           <button type="submit"><i class="fa fa-shopping-bag"></i> ADD TO CART</button>
         </form>
         <ul>
           <li><span>Game ID:</span> COD MMII</li>
-          <li><span>Genre:</span> <a href="#">Action</a>, <a href="#">Team</a>, <a href="#">Single</a></li>
+          <li>
+    <span>Genre:</span>
+    <?php
+    $genres = get_the_terms( get_the_ID(), 'genre' );
+    
+    if ( $genres && ! is_wp_error( $genres ) ) {
+        $genre_links = array();
+        
+        foreach ( $genres as $genre ) {
+            
+            $genre_links[] = '<a href="' . esc_url( get_term_link( $genre ) ) . '">' . esc_html( $genre->name ) . '</a>';
+        }
+        
+        echo implode( ', ', $genre_links );
+    }
+    ?>
+</li>
+
           <li><span>Multi-tags:</span> <a href="#">War</a>, <a href="#">Battle</a>, <a href="#">Royal</a></li>
         </ul>
       </div>
