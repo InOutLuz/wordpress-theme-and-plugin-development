5500df4275566132a0f6b7ae268ed85c10b6eb1a
unknown <wardren@gmail.com>
2024-02-02 10:04:36 +0000

    changed the fallback svg font-size to rem so it's responsive

--- wp-content/themes/final-project-softuni/template-parts/fallback-svg-content.php
+++ wp-content/themes/final-project-softuni/template-parts/fallback-svg-content.php
@@ -12,4 +12,4 @@ if ( empty( $custom_text ) ) {
 		<rect width="100%" height="100%" fill="#3498db"></rect>
 		
 		<!-- Text -->
-		<text x="50%" y="50%" fill="#ffffff" dominant-baseline="middle" text-anchor="middle" font-size="24" font-family="Arial"><?php echo esc_html( $custom_text ); ?></text>
+		<text x="50%" y="50%" fill="#ffffff" dominant-baseline="middle" text-anchor="middle" font-size="0.89rem" font-family="Arial"><?php echo esc_html( $custom_text ); ?></text>
